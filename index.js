15e440fb2b516e06a6099f154c07379a79578c43
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-07-27 10:32:59 -0300

    Borré carpeta img. Agregue js para sacar panel img y trabajar en ese espacio con el panel texto.

---
+++ index.js
@@ -0,0 +1,6 @@
+const botonImg = document.querySelector(".boton-imagen-nav");
+const panelImg = document.getElementById("panel-img");
+
+botonImg.onclick = () => {
+  panelImg.style.width = "0";
+};
