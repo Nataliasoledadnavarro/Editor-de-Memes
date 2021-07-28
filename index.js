f59ad185ce56c2564dac935271b4a8ebf195e257
GabytaDev <gabyta.loyola@gmail.com>
2021-07-28 11:35:44 -0300

    resolvimos funcion url

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ const panelImg = document.getElementById("panel-img");
 botonImg.onclick = () => {
   panelImg.style.width = "0";
 };
+
+//funcion de url imagen arreglar en enter
+
+const imagenMeme = document.getElementById("img-meme");
+const urlInput = document.getElementById("url");
+
+urlInput.onchange = () => {
+  imagenMeme.src = urlInput.value;
+};
+
+console.log(urlInput);
