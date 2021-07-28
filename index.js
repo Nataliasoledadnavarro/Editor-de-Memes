3b28baf8b1a23c9296d619d4e257fc02ed727db5
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-07-28 17:28:51 -0300

    elimino archivos desactualizados

--- index.js
+++
@@ -1,6 +0,0 @@
-const botonImg = document.querySelector(".boton-imagen-nav");
-const panelImg = document.getElementById("panel-img");
-
-botonImg.onclick = () => {
-  panelImg.style.width = "0";
-};
