83c76b26bfb055a98d7a950a396fd1cba1e0fb9e
Natalia Soledad Navarro <nataliasoledadnavarro@gmail.com>
2021-07-29 14:21:31 -0300

    Agrego estilos de inputs rango

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ urlInput.onchange = () => {
 
 console.log(urlInput);
 
-
 // Funciones Input de Rango
 
-const brilloInput = document.getElementById("rango-brillo")
-const opacidadInput = document.getElementById("rango-opacidad")
-const contrasteInput = document.getElementById("rango-contraste")
-const desenfoqueInput = document.getElementById("rango-deenfoque")
-const escalaDeGrisesInput = document.getElementById("rango-grises")
-const sepiaInput = document.getElementById("rango-hue")
-const saturadoInput = document.getElementById("rango-saturado")
-const negativoInput = document.getElementById("rango-negativo")
+const brilloInput = document.getElementById("rango-brillo");
+const opacidadInput = document.getElementById("rango-opacidad");
+const contrasteInput = document.getElementById("rango-contraste");
+const desenfoqueInput = document.getElementById("rango-deenfoque");
+const escalaDeGrisesInput = document.getElementById("rango-grises");
+const sepiaInput = document.getElementById("rango-hue");
+const saturadoInput = document.getElementById("rango-saturado");
+const negativoInput = document.getElementById("rango-negativo");
 
+console.log(brilloInput.value);
