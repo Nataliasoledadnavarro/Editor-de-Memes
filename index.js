c04950e5e79454e921967b129718fe801513e1ba
GabytaDev <gabyta.loyola@gmail.com>
2021-07-30 18:04:38 -0300

    trabajando en selector

--- index.js
+++ index.js
@@ -22,28 +22,59 @@ let textoColorFondo = document.querySelector(".texto-color-fondo-img"); //selecc
 
 const colorPicker= (event) => {
   textoColorFondo.textContent = event.target.value; //toma al valor del evento input y cambiale el contenido de texto a textoColorfondo (target: objetivo)
+  applySelector()
 }
 
 inputColor.addEventListener("input", colorPicker); 
 
-//funcion selector
+//funcion selector y cambio de fondo
 
 const elementoSelector = document.getElementById("opciones-fondo-img"); //seleciono el sector
-const imagenFondo = document.querySelector(".imagen-src")
+const imagenFondo = document.querySelector(".imagen-fondo");
+let selectorFiltro = ""
+let divImagen = document.querySelector(".imagen-src");
 
 const seleccionUsuario = (event)=>{
   if (event.target.value === "aclarar") {
+    console.log("Entre en aclara")
+    imagenFondo.style.backgroundColor = textoColorFondo
     imagenFondo.style.mixBlendMode = "lighten"
   }
+  else if (event.target.value === "oscurecer"){
+    console.log("Entre en oscuro")
+    imagenFondo.style.mixBlendMode = "darken"
+  }
+  else if (event.target.value === "diferencia"){
+    console.log("Entre en differencia")
+    imagenFondo.style.mixBlendMode = "difference"
+  }
+  else if (event.target.value === "luminosidad"){
+    console.log("Entre en lumin")
+    //imagenFondo.style.backgroundBlendMode = textoColorFondo
+    //imagenFondo.style.mixBlendMode = "luminosity"
+    selectorFiltro = "luminosity"
+    applySelector()
+  }
+  else if (event.target.value === "multiplicar"){
+    console.log("Entre en multi")
+    imagenFondo.style.mixBlendMode = "multiply"
+  }
+  else{
+    console.log("Entre en normal")
+    imagenFondo.style.mixBlendMode = "normal"
+  }
+
 }
 
-// object.style.mixBlendMode = "darken"
+const applySelector = () => {
+  console.log("Entre en aplicador de seleccion")
+  //imagenFondo.style.backgroundBlendMode = selectorFiltro
+  divImagen.style.backgroundColor = "0,0,0";
+  imagenFondo.style.mixBlendMode = selectorFiltro;
+}
 
 elementoSelector.addEventListener("change", seleccionUsuario); //escucho cuando se hace click en el sector
 
-
-
-console.log(urlInput);
 
 // Funciones Input de Rango
 
@@ -58,8 +89,8 @@ const saturadoInput = document.getElementById("rango-saturado");
 const negativoInput = document.getElementById("rango-negativo");
 
 const inputsRangeEnCero = () => {
-  brilloInput.value = "0";
-  opacidadInput.value = "0";
+  brilloInput.value = "1";
+  opacidadInput.value = "1";
   contrasteInput.value = "0";
   desenfoqueInput.value = "0";
   escalaDeGrisesInput.value = "0";
