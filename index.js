cb1b88d21945583a18bad1e51d66ec95464b1cfd
GabytaDev <gabyta.loyola@gmail.com>
2021-07-30 11:18:56 -0300

    eventos de picker y select

--- index.js
+++ index.js
@@ -14,4 +14,29 @@ urlInput.onchange = () => {
   imagenMeme.src = urlInput.value;
 };
 
-console.log(urlInput);
+
+// funcion picker de color
+
+let inputColor = document.getElementById("color-fondo-img"); //selecciono el id de input
+let textoColorFondo = document.querySelector(".texto-color-fondo-img"); //selecciono la clase del texto
+
+const colorPicker= (event) => {
+  textoColorFondo.textContent = event.target.value; //toma al valor del evento input y cambiale el contenido de texto a textoColorfondo (target: objetivo)
+}
+
+inputColor.addEventListener("input", colorPicker); 
+
+//funcion selector
+
+const elementoSelector = document.getElementById("opciones-fondo-img"); //seleciono el sector
+
+const seleccionUsuario = (event)=>{
+  if (event.target.value === "aclarar") {
+    // debe de aplicar el filtro a la imagen con el color
+  }
+}
+
+elementoSelector.addEventListener("change", seleccionUsuario); //escucho cuando se hace click en el sector
+
+
+
